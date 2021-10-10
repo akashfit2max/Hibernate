b5bab95c561a7818a536393165ba35fd0d286b9a
Sahoo, Akash K <akashnikz@gmail.com>
2021-10-10 09:55:04 +0530

    adding object in the table

--- ProjectWithHibernate/src/main/java/com/spring/hibernate/App.java
+++ ProjectWithHibernate/src/main/java/com/spring/hibernate/App.java
@@ -1,6 +1,8 @@
 package com.spring.hibernate;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -11,11 +13,26 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Project started");
 
-//      getting the object of session factory / session factory is an interface
+//      getting the object of sessionfactory / sessionfactory is an interface
 		SessionFactory factory = new Configuration().configure().buildSessionFactory();
 
-		System.out.println(factory);
-		System.out.println(factory.isClosed());
+//		System.out.println(factory);
+//		System.out.println(factory.isClosed());
+
+		Student student = new Student(2, "ashish", "delhi");
+
+//		1st way = opening a session
+		/*
+		 * Session session = factory.openSession(); session.save(student);
+		 * session.beginTransaction().commit(); session.close();
+		 */
+
+//		2nd way
+		Session session = factory.openSession();
+		Transaction transaction = session.beginTransaction();
+		session.save(student);
+		transaction.commit();
+		session.close();
 
 	}
 }
